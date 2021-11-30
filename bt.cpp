f64d6263bc37c6ca7cc418a210813027931b093c
Saad Rahman <saadreh5@gmail.com>
2021-12-01 02:01:20 +0500

    implemented all the traversals in a binary tree

--- bt.cpp
+++ bt.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+/*
+
+preorder =  root     left  right (4,1,5,2,6)
+
+postorder =          left right root (5,2,1,6,4 )
+
+inorder =               left root right (5,1,2,4,6)
+
+
+
+        root->4
+            /   \
+            1    6
+           / \
+           5  2
+
+
+
+*/
+
 struct node
 {
     int data;
@@ -21,6 +41,10 @@ class bt
     }
 
     node *create(int);
+
+    void preorder(node *rot);
+    void postorder(node *rot);
+    void inorder(node *rot);
 
 
 };
@@ -48,50 +72,123 @@ node* bt:: create(int x)
 
 }
 
+
+void bt :: preorder(node *rot)
+{
+    if(rot != NULL)
+    {
+    cout<<rot->data<<" ";
+    }
+
+    if(rot -> left != NULL)
+    {
+    preorder(rot->left);
+
+    }
+
+    if(rot -> right != NULL)
+    {
+    
+    preorder(rot -> right);
+
+    }
+    return;
+    
+}
+
+void bt:: postorder(node *rot)
+{
+    if(rot -> left != NULL)
+    {
+        postorder(rot -> left);
+    }
+
+    if(rot -> right != NULL)
+    {
+        postorder(rot -> right);
+
+    }
+
+    cout<< rot ->data << " ";
+    return;
+
+
+   
+
+
+
+}
+
+void bt :: inorder(node *rot)
+{
+    if(rot -> left != NULL)
+    {
+        inorder(rot -> left);
+    }
+
+    cout << rot -> data<<" ";
+
+    if(rot -> right != NULL)
+    {
+        inorder( rot -> right);
+    }
+
+    
+
+    return;
+
+    
+
+
+    
+
+
+   
+    
+    
+   
+}
+
 int main()
 {
     bt obj;
 
-    node *r,*c1,*c2, *n;
+    node *r,*c1,*c2, *c3, *c4;
 
-    //creating the root node and its children
-
-    r = obj.create(2);
+    r = obj.create(4);
     c1 = obj.create(1);
-    c2 = obj.create(4);
+    c2 = obj.create(6);
+    c3 = obj.create(5);
+    c4 = obj.create(2);
 
-    r->left = c1;
+    r -> left = c1;
     r -> right = c2;
 
-    //creating children of children
+    c1 -> left = c3;
+    c1 -> right = c4;
 
-    //creating children of left child
-
-    n = obj.create(11);
-    n->left = NULL;
-    n -> right = NULL;
-    c1 -> left = n;
-
-    n = obj.create(12);
-    n->left = NULL;
-    n -> right = NULL;
-    c1 -> right = n;
-
-    c1 -> right = n;
-
-    //creating children for right child
-
+    c2 -> right = NULL;
     c2 -> left = NULL;
+    //the tree is made 
 
-    n = obj.create(3);
+    obj.preorder(r);
+    cout<< endl; // preorder traversal
+
+    obj.postorder(r);
+    cout<<endl; //postorder traversal
+
+    obj.inorder(r); // inorder traversal
+
+    cout<<endl;
+
     
 
-    n->left = NULL;
-    n->right = NULL;
 
-    c2 -> right = n;
 
-    n = NULL;
+
+    
+
+    
 
 
 

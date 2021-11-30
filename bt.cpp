5dae8483fad4fd080f324e136d39ba9eca510fdf
Saad Rahman <saadreh5@gmail.com>
2021-11-30 23:53:11 +0500

    changing the int main values and structure

--- bt.cpp
+++ bt.cpp
@@ -17,14 +17,15 @@ class bt
     bt()
     {
         root = NULL;
+        newNode = NULL;
     }
 
-    void create(int);
+    node *create(int);
 
 
 };
 
-void bt:: create(int x)
+node* bt:: create(int x)
 {
 
     if(root == NULL)
@@ -34,11 +35,67 @@ void bt:: create(int x)
         root -> left = NULL;
         root -> right = NULL;
 
+        return root;
+
     
     }
+
+    newNode = new node;
+    newNode -> data = x;
+    newNode -> left = NULL;
+    newNode -> right = NULL;
+    return newNode;
+
 }
 
 int main()
 {
+    bt obj;
+
+    node *r,*c1,*c2, *n;
+
+    //creating the root node and its children
+
+    r = obj.create(2);
+    c1 = obj.create(1);
+    c2 = obj.create(4);
+
+    r->left = c1;
+    r -> right = c2;
+
+    //creating children of children
+
+    //creating children of left child
+
+    n = obj.create(11);
+    n->left = NULL;
+    n -> right = NULL;
+    c1 -> left = n;
+
+    n = obj.create(12);
+    n->left = NULL;
+    n -> right = NULL;
+    c1 -> right = n;
+
+    c1 -> right = n;
+
+    //creating children for right child
+
+    c2 -> left = NULL;
+
+    n = obj.create(3);
+    
+
+    n->left = NULL;
+    n->right = NULL;
+
+    c2 -> right = n;
+
+    n = NULL;
+
+
+
+
+    
 
 }
